b8b63f29801a0fc84bb895afc6733147d693972a
savram674 <savram674@gmail.com>
2022-05-02 15:20:54 +0530

    maven java committed

---
+++ src/main/java/com/kmit/maven/maven_example_jar/App.java
@@ -0,0 +1,13 @@
+package com.kmit.maven.maven_example_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
